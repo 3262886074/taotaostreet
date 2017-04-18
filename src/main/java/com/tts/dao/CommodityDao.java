5637d15f9d4abef6b10a3ae738b8ef8f3416ba56
1099185714 <1099185714@qq.com>
2017-04-18 10:19:31 +0800

    0

--- src/main/java/com/tts/dao/CommodityDao.java
+++ src/main/java/com/tts/dao/CommodityDao.java
@@ -1,7 +1,16 @@
 package com.tts.dao;
 
+import com.tts.bean.Commodity;
+
 public interface CommodityDao {
 
+	/**
+	 * 通过商品ID获得商品基本信息
+	 * @param cid
+	 * @return
+	 */
+	public Commodity getById(Integer cid);
+	
 	
 	
 }
