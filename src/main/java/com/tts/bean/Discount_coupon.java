5a6d291fa60c47f50b4d75e7ba850ccab7ade220
yang957862615 <957862615@qq.com>
2017-04-18 22:42:11 +0800

    Revert "Merge branch 'yls-stage01'"
    This reverts commit eded7f60f89b49f165aac19a77707fc1f7c7b1a0, reversing
    changes made to 0153497ebbbda27fbe7c04a73eb6b4b2a6fa6f7b.

--- src/main/java/com/tts/bean/Discount_coupon.java
+++ src/main/java/com/tts/bean/Discount_coupon.java
@@ -14,7 +14,6 @@ public class Discount_coupon implements Serializable {
     private long dcId;
     private Integer condition;        //优惠券条件
     private double reduce;        //减多少钱
-    private Integer status; //优惠券状态 0为未使用 1为已使用
 
     public Discount_coupon() {
     }
@@ -25,7 +24,6 @@ public class Discount_coupon implements Serializable {
                 "dcId=" + dcId +
                 ", condition=" + condition +
                 ", reduce=" + reduce +
-                ", status=" + status +
                 '}';
     }
 
@@ -61,13 +59,5 @@ public class Discount_coupon implements Serializable {
 
     public void setReduce(double reduce) {
         this.reduce = reduce;
-    }
-
-    public Integer getStatus() {
-        return status;
-    }
-
-    public void setStatus(Integer status) {
-        this.status = status;
     }
 }
